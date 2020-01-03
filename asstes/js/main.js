dfc301e5afe618305f7bf2e186befac3c684d3db
Rajat <rajatgour95@gmail.com>
2020-01-03 16:27:11 +0530

    JS added

---
+++ asstes/js/main.js
@@ -0,0 +1,6 @@
+menuToggle = () => {
+    let nav1 = document.getElementById("menu-overlay");
+    nav1.classList.toggle("active");
+    let nav2 = document.getElementById("toggle-icon");
+    nav2.classList.toggle("active");
+}
