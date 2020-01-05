3161de5fb4fbca80322362606517502f10ae5018
Rajat <rajatgour95@gmail.com>
2020-01-05 15:11:21 +0530

    video added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,14 @@ menuToggle = () => {
     let nav2 = document.getElementById("toggle-icon");
     nav2.classList.toggle("active");
 }
+
+// setTimeout(function () {
+//     document.getElementById("video").play();
+// }, 5000);
+
+var video = document.getElementById("video");
+video.addEventListener("canplay", function () {
+    setTimeout(function () {
+        video.play();
+    }, 5000);
+});
